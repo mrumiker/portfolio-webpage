4bb8b0dfdb0753f65ae93e09faca907cf9dbbb61
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-04-18 15:04:26 -0500

    add Hamburger menu to index page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+const hidden = document.querySelector(".hidden");
+const lines = document.querySelectorAll("span");
+document.querySelector(".menu").addEventListener("click", clickHandler)
+function clickHandler() {
+  hidden.classList.toggle("expose");
+  document.querySelector("span:nth-child(1)").classList.toggle("change")
+  document.querySelector("span:nth-child(2)").classList.toggle("change")
+  document.querySelector("span:nth-child(3)").classList.toggle("change")
+}
+
 // (function() {
 //   let form = document.querySelector('#contact-form');
 //   let emailInput = document.querySelector('#contact-email');
